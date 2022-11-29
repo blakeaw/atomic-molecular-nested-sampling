f40321e09ff0ee529ab0a13c7917032b01ebabc5
blakeaw <blakeaw1102@gmail.com>
2022-11-28 20:29:24 -0600

    First commit with one class file.

---
+++ src/classes/Bead.cpp
@@ -0,0 +1,16 @@
+//=================================
+// class definition
+#include "/net/uu/nm/cm/bxw109120/NestedSampling/Lipid/src/class/Bead.h"
+
+//=================================
+//Dependencies
+#include "/net/uu/nm/cm/bxw109120/NestedSampling/Lipid/src/class/CGLipid.h"
+
+
+//=================================
+// the class functions
+
+void Bead::SetMolec(CGLipid * m){
+			molec = m;
+			return;
+		}
